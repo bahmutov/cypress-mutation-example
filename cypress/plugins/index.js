0d148a23bd92863f8c7864c24cd1284d9b3d0790
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2021-01-03 18:20:33 -0500

    remove component testing plugin

--- cypress/plugins/index.js
+++ cypress/plugins/index.js
@@ -3,7 +3,6 @@
  * @type {Cypress.PluginConfig}
  */
 module.exports = (on, config) => {
-  require('cypress-react-unit-test/plugins/cra-v3')(on, config)
   // IMPORTANT to return the config object
   // with the any changed environment variables
   return config
